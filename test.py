443385389226504a9704194fd747c15961a82830
Jimmy Chang <zbwgychame@gmail.com>
2020-04-01 21:57:57 +0800

    Update test.py
    Modified the model loading way

--- test.py
+++ test.py
@@ -9,25 +9,6 @@ from utils import *
 from option import args
 from model.__init__ import make_model
 
-import argparse
-
-parser = argparse.ArgumentParser(description='SADNET')
-# File paths
-parser.add_argument('--gt_src_path', type=str, default="./dataset/test/",
-                    help='testing clear image path, if not, set None')
-parser.add_argument('--noise_src_path', type=str, default="./dataset/test/color_sig50/",
-                    help='testing noisy image path')
-parser.add_argument('--test_items', default=["CBSD68", "Kodak24"],
-                    help='testing dataset')
-parser.add_argument('--result_png_path', type=str, default="./result/SADNET_color_sig50/",
-                    help='result directory')
-parser.add_argument('--ckpt_dir', type=str, default="./ckpt/SADNET_color_sig50/",
-                    help='model directory')
-
-# Hardware specifications
-parser.add_argument('--gpu', type=str, default="0",
-                    help='GPUs')
-args = parser.parse_args()
 
 def evaluate_net():
 
@@ -40,7 +21,7 @@ def evaluate_net():
     for item in args.test_items:
         noise_tmp = sorted(glob.glob(args.noise_src_path + item + '/'))
         noise_src_folder_list.extend(noise_tmp)
-        dst_png_path_list.append(args.result_png_path + item + '/'])
+        dst_png_path_list.append(args.result_png_path + item + '/')
         if args.gt_src_path:
             gt_src_folder_list.extend(sorted(glob.glob(args.gt_src_path + item + '/')))
 
@@ -49,11 +30,19 @@ def evaluate_net():
         ssim = np.zeros(len(gt_src_folder_list))
     test_time = np.zeros(len(noise_src_folder_list))
 
+    # Build model
+    if args.gray:
+        input_channel, output_channel = 1, 1
+    else:
+        input_channel, output_channel = 3, 3
+
+    model = make_model(input_channel, output_channel, args)
+
     if torch.cuda.is_available():
-        model = torch.load(ckpt_dir + 'model_*.pth')
+        model_dict = torch.load(args.ckpt_dir_test+'model_%04d_dict.pth' % args.epoch_test)
+        model.load_state_dict(model_dict)
         model = model.cuda()
     else:
-        #model = torch.load(ckpt_dir + 'model_*.pth', map_location='cpu')
         print('There are not available cuda devices !')
 
     model.eval()
@@ -72,10 +61,11 @@ def evaluate_net():
                 clean = imread(gt_files[ind]).astype(np.float32) / 255
                 clean = clean[0:(clean.shape[0]//8)*8, 0:(clean.shape[1]//8)*8]
 
-            img_test = imread(in_files[ind]).astype(np.float32) / 255
-            img_test = img_test[0:(img_test.shape[0]//8)*8, 0:(img_test.shape[1]//8)*8]
+            img_name = os.path.split(in_files[ind])[-1].split('.')[0]
+            noisy = imread(in_files[ind]).astype(np.float32) / 255
+            noisy = noisy[0:(noisy.shape[0]//8)*8, 0:(noisy.shape[1]//8)*8]
 
-            img_test = transforms.functional.to_tensor(img_test)
+            img_test = transforms.functional.to_tensor(noisy)
             img_test = img_test.unsqueeze_(0).float()
             if torch.cuda.is_available():
                 img_test = img_test.cuda()
@@ -88,26 +78,24 @@ def evaluate_net():
             if ind > 0:
                 test_time[i] += (time.time() - start_time)
             print("took: %4.4fs" % (time.time() - start_time))
-            print("process folder:%s" % src_folder_list[i])
+            print("process folder:%s" % noise_src_folder_list[i])
             print("[*] save images")
 
             rgb = out_image.cpu().detach().numpy().transpose((0,2,3,1))
-            if img_test.ndim == 3:
+            if noisy.ndim == 3:
                 rgb = np.clip(rgb[0], 0, 1)
-            elif img_test.ndim == 2:
+            elif noisy.ndim == 2:
                 rgb = np.clip(rgb[0, :, :, 0], 0, 1)
 
-            rgb = np.uint8(rgb*255)
             # save image
-            imwrite(dst_png_path_list[i] + img_name + ".png", rgb)
+            imwrite(dst_png_path_list[i] + img_name + ".png", np.uint8(rgb*255))
 
             if args.gt_src_path:
-                clean = np.uint8(clean*255)
 
                 psnr[i] += compare_psnr(clean, rgb)
 
                 if clean.ndim == 2:
-                    ssim[i] += compare_ssim(clean, rgb))
+                    ssim[i] += compare_ssim(clean, rgb)
                 elif clean.ndim == 3:
                     ssim[i] += compare_ssim(clean, rgb, multichannel=True)
 
